7f7c985904f0cd6aef3258abb46544bc5727d1dd
Marcos Anjos <dev.marcosanjos@gmail.com>
2024-02-12 22:36:27 -0300

    Finalizando Sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,21 @@
+import { Header } from "./Components/Header"
 import { Post } from "./Post"
+import './global.css'
+import styles from './App.module.css'
+import { Sidebar } from "./Components/Sidebar"
 
 function App() {
   return (
     <div>
-       <Post 
-      author="Marcos Alexandre" 
-      content="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nobis, sit pariatur consequuntur dolorum assumenda est ipsam nostrum a fugiat in ut dolorem fuga numquam. Veritatis laboriosam distinctio corporis incidunt fuga!"
-    />
-    <Post 
-      author="Segundo Autor" 
-      content="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nobis, sit pariatur consequuntur dolorum assumenda est ipsam nostrum a fugiat in ut dolorem fuga numquam. Veritatis laboriosam distinctio corporis incidunt fuga!"
-    />
+      <Header />
+
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          <Post author="Marcos Alexandre" content="Hello, world!" />
+          <Post author="Marcos Anjos" content="Hi, world!" />
+        </main>
+      </div>
     </div>
   )
 }
---
+++ src/Components/Sidebar.jsx
@@ -0,0 +1,26 @@
+import styles from './Sidebar.module.css';
+import {PencilLine} from 'phosphor-react'
+
+export function Sidebar() {
+  return (
+    <aside className={styles.sidebar}>
+        <img
+            className={styles.cover} 
+            src="https://images.unsplash.com/photo-1534972195531-d756b9bfa9f2?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" 
+        />
+
+        <div className={styles.profile}>
+            <img className={styles.avatar} src="https://github.com/anjosmarcos.png" />
+            <strong>Marcos Alexandre</strong>
+            <span>Web Developer</span>
+        </div>
+
+        <footer>
+            <a href="#">
+                <PencilLine size={20} />
+                Editar seu perfil
+            </a>
+        </footer>
+    </aside>
+  )
+}
